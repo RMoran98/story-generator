d3e20161f713335456b46e4cc9695d3ccd1b45dc
RMoran98 <moranrachel359@gmail.com>
2023-08-20 13:28:37 +0100

    Create storeWords function and relevant docstring

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Default attributes, so the user will not have errors or blank spaces in the generated story if they click on a story button without submitting any words
+// Default variables for the words, so the user will not have errors or blank spaces in the generated story if they click on a story button without submitting anything
 let adjective = "clever";
 let color = "green";
 let food = "apple";
@@ -18,3 +18,16 @@ document.addEventListener("DOMContentLoaded", function () {
         })
     }
 })
+/**
+ * Gets the words the user types in and stores them as variables, overwiting the default values, to be used in the story functions. For testing purposes, the values are then logged to the console.
+ */
+function storeWords() {
+    adjective = document.getElementById(`adjective`).innerText;
+    color = document.getElementById(`color`).innerText;
+    food = document.getElementById(`food`).innerText;
+    job = document.getElementById(`job`).innerText;
+    console.log(`Adjective is ${adjective}`);
+    console.log(`Color is ${color}`);
+    console.log(`Food is ${food}`);
+    console.log(`Job is ${job}`);
+}
