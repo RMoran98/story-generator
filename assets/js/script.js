4ce15baeda77f0a7167d0e430a8927eed301463b
RMoran98 <moranrachel359@gmail.com>
2023-08-20 13:08:00 +0100

    Add default attributes as variables and add event listeners for button clicks once the DOM has loaded

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// Default attributes, so the user will not have errors or blank spaces in the generated story if they click on a story button without submitting any words
+let adjective = clever;
+let color = green;
+let food = apple;
+let job = accountant;
+// Wait for the DOM to finish loading, then listen for button click events
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("Saving words!");
+            } else {
+                let story = this.getAttribute("data-type"); {
+                    alert(`You chose ${story}!`)
+                }
+            }
+        })
+    }
+})
